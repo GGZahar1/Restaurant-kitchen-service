8dae446e80546b22cbf6a147962b3a98e9be1baa
Zahar <galushkozahar@gmail.com>
2026-09-21 20:39:26 +0300

    Add cook, dish and dish_type list pages

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, CookListView, DishTypeListView, DishListView
 
 urlpatterns = [
     path("", index, name="home"),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
+    path("dish_types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dishes/", DishListView.as_view(), name="dish-list"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Dish, DishType, Cook
 
@@ -13,3 +14,22 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_cooks": get_user_model().objects.count()
     }
     return render(request, "kitchen/index.html", context=context)
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    paginate_by = 6
+    template_name = "kitchen/cook-list.html"
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    paginate_by = 6
+    template_name = "kitchen/dish-type-list.html"
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    paginate_by = 6
+    template_name = "kitchen/dish-list.html"
+
