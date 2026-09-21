e11f3535f6961b8fb0c17ce790e73d8ee21fd33c
Zahar <galushkozahar@gmail.com>
2026-09-21 18:28:16 +0300

    Create home page and base template

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish, DishType, Cook
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        "num_dish": Dish.objects.count(),
+        "num_dish_type": DishType.objects.count(),
+        "num_cooks": get_user_model().objects.count()
+    }
+    return render(request, "kitchen/index.html", context=context)
